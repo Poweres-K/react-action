1c271909be674e400ed0b0997a6783cfcb3abced
Poweres-K <90740118+Poweres-K@users.noreply.github.com>
2022-06-30 18:32:01 +0700

    test14

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,23 +1,27 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  const token = core.getInput("token");
-  const title = core.getInput("title");
-  const body = core.getInput("body");
-  const assignee = core.getInput("assignee");
+const run = async () => {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignee = core.getInput("assignee");
 
-  const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(token);
 
-  const response = octokit.rest.issues.create({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    title,
-    body,
-    assignee: assignee ? assignee.split(",") : undefined,
-  });
+    const response = await octokit.rest.issues.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title,
+      body,
+      assignee: assignee ? assignee.split(",") : undefined,
+    });
 
-  core.setOutput("issue", JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
-}
+    core.setOutput("issue", JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
